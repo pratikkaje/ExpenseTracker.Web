918da9a58c3e1ba7ed3894f3f60afb5bdd32376d
Pratik Kaje <kajepratik@gmail.com>
2024-03-31 19:54:48 +0400

    Base: Add TextBox Base Component

---
+++ ExpenseTracker.Web/ExpenseTracker.Web/Models/Basics/ComponentState.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+namespace ExpenseTracker.Web.Models.Basics
+{
+    public enum ComponentState
+    {
+        Loading,
+        Content,
+        Error
+    }
+}
